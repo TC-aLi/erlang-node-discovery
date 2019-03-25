7d5ea207a3eac89337e20460d6a210bc8b72280b
Ang Li <ali@tigerconnect.com>
2019-03-25 17:44:27 +0800

    Fix issue where state info is not complete after restarted by sup.

--- src/erlang_node_discovery_pubsub.erl
+++ src/erlang_node_discovery_pubsub.erl
@@ -9,7 +9,7 @@
 -export([code_change/3]).
 
 -export([start_link/0]).
--export([pub/0, pub/3]).
+-export([pub/0]).
 
 -record(state, {
     sub_pid     :: pid(),
@@ -30,46 +30,16 @@ start_link() ->
 
 -spec pub() -> ok.
 pub() ->
-    Conf = application:get_all_env(erlang_node_discovery),
-    DiscoveryPort = proplists:get_value(discovery_port, Conf, 0),
-    ChannelPrefix = proplists:get_value(channel_prefix, Conf, ?DEFAULT_CHANNEL_PREFIX),
-    ContainerName = proplists:get_value(container_name, Conf, ?DEFAULT_CONTAINER_NAME),
-    PubInterval   = proplists:get_value(pub_interval,   Conf, ?DEFAULT_PUB_INTERVAL),
-    Chan = list_to_binary(ChannelPrefix ++ atom_to_list(node())),
-    Payload = term_to_binary({node(), {os:getenv(ContainerName), DiscoveryPort}}),
-    pub(Chan, Payload, PubInterval).
-
--spec pub(string(), term(), integer()) -> ok.
-pub(Chan, Payload, Intvl) ->
-    gen_server:call(?MODULE, {pub, Chan, Payload, Intvl}, infinity).
-
-
--spec psub() -> {pid(), binary()}.
-psub() ->
-    Conf = application:get_all_env(erlang_node_discovery),
-    ChannelPrefix = proplists:get_value(channel_prefix, Conf, ?DEFAULT_CHANNEL_PREFIX),
-    psub(list_to_binary(ChannelPrefix ++ "*")).
-
--spec psub(binary()) -> {pid(), binary()}.
-psub(PChan) ->
-    Conf = proplists:get_value(pubsub_1, redis_config_manager:get_all_hosts(pubsub), []),
-    Host = proplists:get_value(name, Conf, "127.0.0.1"),
-    Port = proplists:get_value(port, Conf, 6379),
-    {ok, Pid} = eredis_sub:start_link(Host, Port, ""),
-    eredis_sub:controlling_process(Pid),
-    eredis_sub:psubscribe(Pid, [PChan]),
-    {Pid, PChan}.
+    gen_server:call(?MODULE, {pub, self()}, infinity).
 
 
 %% gen_server
 init([]) ->
-    {Pid, PChan} = psub(),
-    {ok, #state{sub_pid = Pid, sub_pchan = PChan}}.
+    {ok, init_pub(init_psub(#state{}))}.
 
 
-handle_call({pub, Chan, Payload, Intvl}, _From, State) ->
-    State1 = update_pub_timer(start, State),
-    {reply, ok, State1#state{pub_chan = Chan, pub_payload = Payload, pub_intvl = Intvl}};
+handle_call({pub, _Self}, _From, State) ->
+    {reply, ok, update_pub_timer(start, State)};
 
 handle_call(Msg, _From, State) ->
     io:format("Unexpected message: ~p~n", [Msg]),
@@ -90,8 +60,7 @@ handle_info({pub, _From}, State = #state{pub_payload = Payload}) ->
 
 handle_info(timeout, State = #state{sub_pchan = PChan}) ->
     io:format("timeout ~n"),
-    {Pid, _} = psub(PChan),
-    {noreply, State#state{sub_pid = Pid}};
+    {noreply, State#state{sub_pid = psub(PChan)}};
 
 handle_info({subscribed, PChan, Pid}, State = #state{sub_pid = Pid}) ->
     io:format("Channel subscribed ~p~n", [PChan]),
@@ -133,6 +102,35 @@ code_change(_Old, State, _Extra) ->
 
 
 %% internal funcs
+init_pub(State) ->
+    Conf = application:get_all_env(erlang_node_discovery),
+    DiscoveryPort = proplists:get_value(discovery_port, Conf, 0),
+    ChannelPrefix = proplists:get_value(channel_prefix, Conf, ?DEFAULT_CHANNEL_PREFIX),
+    ContainerName = proplists:get_value(container_name, Conf, ?DEFAULT_CONTAINER_NAME),
+    PubInterval   = proplists:get_value(pub_interval,   Conf, ?DEFAULT_PUB_INTERVAL),
+    Chan = list_to_binary(ChannelPrefix ++ atom_to_list(node())),
+    Payload = term_to_binary({node(), {os:getenv(ContainerName), DiscoveryPort}}),
+    State#state{pub_chan = Chan, pub_payload = Payload, pub_intvl = PubInterval}.
+
+
+init_psub(State) ->
+    Conf = application:get_all_env(erlang_node_discovery),
+    ChannelPrefix = proplists:get_value(channel_prefix, Conf, ?DEFAULT_CHANNEL_PREFIX),
+    PChan = list_to_binary(ChannelPrefix ++ "*"),
+    Pid = psub(PChan),
+    State#state{sub_pid = Pid, sub_pchan = PChan}.
+
+
+psub(PChan) ->
+    Conf = proplists:get_value(pubsub_1, redis_config_manager:get_all_hosts(pubsub), []),
+    Host = proplists:get_value(name, Conf, "127.0.0.1"),
+    Port = proplists:get_value(port, Conf, 6379),
+    {ok, Pid} = eredis_sub:start_link(Host, Port, ""),
+    eredis_sub:controlling_process(Pid),
+    eredis_sub:psubscribe(Pid, [PChan]),
+    Pid.
+
+
 update_pub_timer(start, State) ->
     Self = self(),
     Timer = erlang:send_after(0, Self, {pub, Self}),
